159de8d8032149a5a3ccc0f4049263688af5d3b4
hamidba <balalzadehhamid79@gmail.com>
2023-01-21 01:40:45 +0330

    add post and category models

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import core.settings as sting
 
-# Create your models here.
+class Post(models.Model):
+    """Blog Model"""
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=125)
+    content = models.TextField()
+    status = models.BooleanField()
+    published_data = models.DateTimeField()
+
+    auhtor = models.ForeignKey(sting.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    category = models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+
+    created_data = models.DateTimeField(auto_now_add=True)
+    updated_data = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Category(models.Model):
+    """Category Model"""
+    name = models.CharField(max_length=85)
+
+    def __str__(self) -> str:
+        return self.name
