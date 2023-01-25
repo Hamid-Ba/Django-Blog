134feab7bcef68dbdffcb71217932c480ccc1a9f
hamidba <balalzadehhamid79@gmail.com>
2023-01-25 23:23:18 +0330

    account module model folder clustering

---
+++ core/accounts/models/profile.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Profile(models.Model):
+    """Profile Model"""
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
+    avatar = models.ImageField(null=True, blank=True)
+    bio = models.CharField(max_length=500)
+
+    created_data = models.DateTimeField(auto_now_add=True)
+    updated_data = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.email
--- core/accounts/models/user.py
+++ core/accounts/models/user.py
@@ -44,18 +44,3 @@ class User(AbstractBaseUser,PermissionsMixin):
         return self.email
 
     objects = UserManager()
-
-class Profile(models.Model):
-    """Profile Model"""
-    first_name = models.CharField(max_length=225)
-    last_name = models.CharField(max_length=225)
-    avatar = models.ImageField(null=True, blank=True)
-    bio = models.CharField(max_length=500)
-
-    created_data = models.DateTimeField(auto_now_add=True)
-    updated_data = models.DateTimeField(auto_now=True)
-
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return self.user.email
