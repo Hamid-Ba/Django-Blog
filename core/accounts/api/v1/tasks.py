a74cb16f0803acd1fc47270560a40f619b290b47
hamidba <balalzadehhamid79@gmail.com>
2023-01-28 02:16:38 +0330

    add celery beat
    add celery beat to make scheduling

--- core/accounts/api/v1/tasks.py
+++ core/accounts/api/v1/tasks.py
@@ -8,3 +8,6 @@ def send_activation_email(subject,to_email,token):
     The Verification URL For You : http://127.0.0.1:8000/accounts/api/v1/verification/{token}/
     """
     send_mail(subject, message, 'balalzadehhamid79@gmail.com',[to_email], fail_silently=False)
+
+@shared_task
+def my_sum(x,y) : return x + y
