675a7f39a364b194e58944de7b134db7276dbc8f
hamidba <balalzadehhamid79@gmail.com>
2023-02-06 07:50:59 +0330

    dockeize the project 😁

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,23 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
+from decouple import config
 from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^f9+9p&8!1*hv$=a9i-@gz34qzgcu1+&x2s-mfyk3n%#k@(4ao'
+SECRET_KEY = config("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
